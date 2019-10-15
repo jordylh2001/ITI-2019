d3f7fa6b0133317474b786260413ed4759690668
jordylh2001 <1930136@upv.edu.mx>
2019-10-15 12:46:59 -0500

    rutas.html

--- default/full/index2.php
+++ default/full/index2.php
@@ -418,33 +418,32 @@
 							<a href="#" class="nav-link"><i class="icon-color-sampler"></i> <span>Alumnos</span></a>
 
 							<ul class="nav nav-group-sub" data-submenu-title="Themes">
-								<li class="nav-item"><a href="../../../LTR/default/full/form_layout_horizontal.html" class="nav-link active">Alta </a></li>
-								<li class="nav-item"><a href="../../../LTR/default/full/datatable_basic.html" class="nav-link ">Consultar </a></li>
-								<li class="nav-item"><a href="../../../LTR/default/full/rutas.html" class="nav-link ">rutas </a></li>
+								<li class="nav-item"><a href="../../../LTR/default/full/form_layout_horizontal.php" class="nav-link active">Alta </a></li>
+								<li class="nav-item"><a href="../../../LTR/default/full/datatable_basic.php" class="nav-link ">Consultar </a></li>
 							</ul>
 						</li>
 						<li class="nav-item nav-item-submenu">
 							<a href="#" class="nav-link"><i class="icon-color-sampler"></i> <span>Maestros</span></a>
 
 							<ul class="nav nav-group-sub" data-submenu-title="Themes">
-								<li class="nav-item"><a href="../../../LTR/default/full/form_layout_horizontal.html" class="nav-link active">Alta </a></li>
-								<li class="nav-item"><a href="../../../LTR/default/full/datatable_basic.html" class="nav-link ">Consultar </a></li>
+								<li class="nav-item"><a href="../../../LTR/default/full/form_layout_horizontal.php" class="nav-link active">Alta </a></li>
+								<li class="nav-item"><a href="../../../LTR/default/full/datatable_basic.php" class="nav-link ">Consultar </a></li>
 							</ul>
 						</li>
 						<li class="nav-item nav-item-submenu">
 							<a href="#" class="nav-link"><i class="icon-color-sampler"></i> <span>Materias</span></a>
 
 							<ul class="nav nav-group-sub" data-submenu-title="Themes">
-								<li class="nav-item"><a href="../../../LTR/default/full/form_layout_horizontal.html" class="nav-link active">Alta </a></li>
-								<li class="nav-item"><a href="../../../LTR/default/full/datatable_basic.html" class="nav-link ">Consultar </a></li>
+								<li class="nav-item"><a href="../../../LTR/default/full/form_layout_horizontal.php" class="nav-link active">Alta </a></li>
+								<li class="nav-item"><a href="../../../LTR/default/full/datatable_basic.php" class="nav-link ">Consultar </a></li>
 							</ul>
 						</li>	
 						<li class="nav-item nav-item-submenu">
 							<a href="#" class="nav-link"><i class="icon-color-sampler"></i> <span>Carreras</span></a>
 
 							<ul class="nav nav-group-sub" data-submenu-title="Themes">
-								<li class="nav-item"><a href="../../../LTR/default/full/form_layout_horizontal.html" class="nav-link active">Alta </a></li>
-								<li class="nav-item"><a href="../../../LTR/default/full/datatable_basic.html" class="nav-link ">Consultar </a></li>
+								<li class="nav-item"><a href="../../../LTR/default/full/form_layout_horizontal.php" class="nav-link active">Alta </a></li>
+								<li class="nav-item"><a href="../../../LTR/default/full/datatable_basic.php" class="nav-link ">Consultar </a></li>
 							</ul>
 						</li>	
 									<!-- >	
@@ -1379,52 +1378,7 @@
 
 					<div class="col-xl-5">
 
-						<!-- Sales stats -->
-						<div class="card">
-							<div class="card-header header-elements-inline">
-								<h6 class="card-title">Sales statistics</h6>
-								<div class="header-elements">
-									<select class="form-control" id="select_date" data-fouc>
-										<option value="val1">June, 29 - July, 5</option>
-										<option value="val2">June, 22 - June 28</option>
-										<option value="val3" selected>June, 15 - June, 21</option>
-										<option value="val4">June, 8 - June, 14</option>
-									</select>
-			                	</div>
-							</div>
-
-							<div class="card-body py-0">
-								<div class="row text-center">
-									<div class="col-4">
-										<div class="mb-3">
-											<h5 class="font-weight-semibold mb-0">5,689</h5>
-											<span class="text-muted font-size-sm">new orders</span>
-										</div>
-									</div>
-
-									<div class="col-4">
-										<div class="mb-3">
-											<h5 class="font-weight-semibold mb-0">32,568</h5>
-											<span class="text-muted font-size-sm">this month</span>
-										</div>
-									</div>
-
-									<div class="col-4">
-										<div class="mb-3">
-											<h5 class="font-weight-semibold mb-0">$23,464</h5>
-											<span class="text-muted font-size-sm">expected profit</span>
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div class="chart mb-2" id="app_sales"></div>
-							<div class="chart" id="monthly-sales-stats"></div>
-						</div>
-						<!-- /sales stats -->
-
-					</div>
-				</div>
+						
 				<!-- /main charts -->
 
 
